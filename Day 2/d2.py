18d59b77968e0911493d0cd60aacf6c472ac56d1
strogera <26325577+strogera@users.noreply.github.com>
2021-02-21 14:08:35 +0200

    Day 2

--- Day 2/d2.py
+++ Day 2/d2.py
@@ -1,11 +1,42 @@
+from itertools import combinations
+from math import prod
+
+def expandSides(dimension):
+    return list(combinations(dimension, 2))
+
+def calculateArea(sides):
+    areas = []
+    for s in sides:
+        areas.append(prod(s))
+    return areas
+
+def calculateWrapPapper(areas):
+    return 2*sum(areas) + min(areas)
+
 def partOne():
     with open("input.txt", "r") as inputFile:
+        totalWrap = 0
         for line in inputFile:
-            #elems=line.strip().split()
+            dim = line.strip().split('x')
+            dim = [int(x) for x in dim]
+            sides = expandSides(dim) 
+            areas = calculateArea(sides)
+            totalWrap += calculateWrapPapper(areas) 
+        return totalWrap
 
+def calculateRibbonPresent(sides):
+    halfperimeter = [sum(sidePair) for sidePair in sides]
+    return min(halfperimeter) * 2
 
 def partTwo():
-    return 'unknown'
+    with open("input.txt", "r") as inputFile:
+        totalRibbon = 0
+        for line in inputFile:
+            dim = line.strip().split('x')
+            dim = [int(x) for x in dim]
+            sides = expandSides(dim) 
+            totalRibbon += calculateRibbonPresent(sides) + prod(dim)
+        return totalRibbon
 
 print("Answer for part 1: ")
 print(partOne())
